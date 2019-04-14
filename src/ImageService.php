2f5605f7e2d1bf9734f6797626783c563b4e2493
Dmitry <d.i.ma.92@mail.ru>
2019-04-14 16:58:55 +0600

    add params resize

--- src/ImageService.php
+++ src/ImageService.php
@@ -4,13 +4,13 @@ namespace Djmitry\Image;
 
 class ImageService
 {
-    public function resize($image)
+    public function resize(string $image, int $w, int $h)
     {
         $images_path = public_path('storage/images/');
         $cache_path = public_path('storage/images/cache/');
         $pathToImage = $images_path . $image;
         $img = \Image::make($pathToImage);
-        $img->fit(800, 200, function($constraint) {
+        $img->fit($w, $h, function($constraint) {
             //$constraint->upSize();
         });
         $img->save($cache_path . $img->basename);
--- src/ImageTrait.php
+++ src/ImageTrait.php
@@ -6,12 +6,12 @@ use Djmitry\Image\ImageService;
 
 trait ImageTrait
 {
-    public function imageUrl()
+    public function image(int $w = 100, int $h = 100)
     {
         $path = '/storage/images/cache';
         if (!file_exists(public_path('storage/images/cache/') . $this->image)) {
             $imageService = new ImageService();
-            $imageService->resize($this->image);
+            $imageService->resize($this->image, $w, $h);
         }
         return asset('storage/images/cache/' . $this->image);
     }
